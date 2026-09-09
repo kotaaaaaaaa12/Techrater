5f4f4e90f9c8b97db0c0e09b7cf5f060aa1d62d5
kotaaaaaaaa12 <kotarohigashi12@gmail.com>
2026-09-09 17:14:50 +0900

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,7 +1,16 @@
 #include <drogon/drogon.h>
+#include <controllers/WebSocket.h>
+
+#include <memory>
 
 int main() {
     drogon::app().loadConfigFile("config.json");
+
+    drogon::app().registerController(
+            std::make_shared<techmino::ws::v1::WebSocket>()
+    );
+    LOG_INFO << "Techrater WebSocket controller registered";
+
     drogon::app().run();
     return 0;
 }
