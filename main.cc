0bb43d5fa1496ee731dc0b306289bb8c3c723052
kotaaaaaaaa12 <kotarohigashi12@gmail.com>
2026-09-09 22:16:47 +0900

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,15 +1,16 @@
 #include <drogon/drogon.h>
 #include <controllers/WebSocket.h>
+#include <memory>
 
 int main() {
     drogon::app().loadConfigFile("config.json");
-    techmino::ws::v1::WebSocket::initPathRouting();
+    drogon::app().registerController(
+            std::make_shared<techmino::ws::v1::WebSocket>()
+    );
 
     drogon::app().registerBeginningAdvice([]() {
         bool routeFound = false;
         for (const auto &handler : drogon::app().getHandlersInfo()) {
-            LOG_INFO << "TECHRATER_ROUTE path=" << std::get<0>(handler)
-                     << " handler=" << std::get<2>(handler);
             if (std::get<0>(handler) == "/techmino/ws/v1" &&
                 std::get<2>(handler).find("WebsocketController") != std::string::npos) {
                 routeFound = true;
@@ -17,7 +18,7 @@ int main() {
         }
 
         if (routeFound) {
-            LOG_INFO << "TECHRATER_WS_ROUTE_READY path=/techmino/ws/v1";
+            LOG_INFO << "TECHRATER_WS_CONTROLLER_BOUND path=/techmino/ws/v1";
         } else {
             LOG_ERROR << "TECHRATER_WS_ROUTE_MISSING path=/techmino/ws/v1";
         }
